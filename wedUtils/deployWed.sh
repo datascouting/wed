ed1f6d4bc914a1297b2248deae183c9c2a2d2ef1
Iordanis Kostelidis <ikostelidis@datascouting.com>
2020-02-24 17:43:20 +0200

    wip

--- wedUtils/deployWed.sh
+++ wedUtils/deployWed.sh
@@ -3,7 +3,7 @@
 BUILD_PATH=/home/node/wed/build
 PUBLIC_PATH="/home/node/vhosts/public"
 
-undeploy \
+cleanWed \
 && cp -r ${BUILD_PATH}/packed/* ${PUBLIC_PATH} \
 && cp -r ${BUILD_PATH}/schemas ${PUBLIC_PATH} \
 && rm ${PUBLIC_PATH}/kitchen-sink.html \
