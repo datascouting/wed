0ce20fc767a74f3deb14b2d14707bf6f3fa4c600
Iordanis Kostelidis <ikostelidis@datascouting.com>
2020-02-25 11:20:13 +0200

    wip

--- hooks/build.sh
+++ hooks/build.sh
@@ -2,8 +2,17 @@
 
 echo "[***] Build hook running"
 
-echo "[***] Build common image"
-docker build -f Dockerfile.common -t wed-ds-common:latest .
-
-echo "[***] Build release image"
-docker build -f Dockerfile.production -t "${IMAGE_NAME}" .
+case "${COMMON_BUILD}" in
+"true")
+    echo "[***] Building the common image" \
+     && docker build -f Dockerfile.common -t "${IMAGE_NAME}" .
+  ;;
+*)
+  echo "[***] Pulling the common image" \
+    && docker pull iordaniskostelidis/wed-common:latest \
+    && echo "[***] Renaming the common image" \
+    && docker tag iordaniskostelidis/wed-common:latest wed-ds-common:latest \
+    && echo "[***] Building the production image" \
+    && docker build -f Dockerfile.production -t "${IMAGE_NAME}" .
+  ;;
+esac
